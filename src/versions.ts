f893adce7844814e17aebdd724c03c15d4f45fbe
Shin'ya UEOKA <ueokande@i-beam.org>
2020-12-29 01:39:17 +0000

    lint:fix

--- src/versions.ts
+++ src/versions.ts
@@ -1,16 +1,16 @@
 export const LatestVersion = {
-  LATEST_BETA: 'latest-beta',
-  LATEST_ESR: 'latest-esr',
-  LATEST: 'latest'
-} as const
+  LATEST_BETA: "latest-beta",
+  LATEST_ESR: "latest-esr",
+  LATEST: "latest",
+} as const;
 
 // eslint-disable-next-line no-redeclare
-export type LatestVersion = typeof LatestVersion[keyof typeof LatestVersion]
+export type LatestVersion = typeof LatestVersion[keyof typeof LatestVersion];
 
 export const isLatestVersion = (version: string): version is LatestVersion => {
   return (
     version === LatestVersion.LATEST ||
     version === LatestVersion.LATEST_BETA ||
     version === LatestVersion.LATEST_ESR
-  )
-}
+  );
+};
