234ab163c2a115da4dd2e29c9363aadc4cd75dd9
Shin'ya UEOKA <ueokande@i-beam.org>
2020-12-29 13:58:07 +0000

    Merge installer.ts to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,48 @@
 import * as core from "@actions/core";
 import * as io from "@actions/io";
+import * as tc from "@actions/tool-cache";
 import cp from "child_process";
-import * as installer from "./installer";
-import { getPlatform } from "./platform";
-import { LatestVersion } from "./versions";
+import { getPlatform, Platform } from "./platform";
+import { LatestVersion, isLatestVersion } from "./versions";
+import { DownloadURLFactory } from "./DownloadURLFactory";
+import { ExtractorFactory } from "./ExtractorFactory";
 
-async function run(): Promise<void> {
+async function install(
+  version: string,
+  platform: Platform,
+  language: string
+): Promise<string> {
+  const cacheEnabled = !isLatestVersion(version);
+  if (cacheEnabled) {
+    const toolPath = tc.find("firefox", version);
+    if (toolPath) {
+      core.info(`Found in cache @ ${toolPath}`);
+      return toolPath;
+    }
+  }
+  core.info(`Attempting to download firefox ${version}...`);
+
+  const url = new DownloadURLFactory(version, platform, language)
+    .create()
+    .getURL();
+  core.info(`Acquiring ${version} from ${url}`);
+
+  const archivePath = await tc.downloadTool(url);
+  core.info("Extracting Firefox...");
+  const extPath = await new ExtractorFactory()
+    .create(platform)
+    .extract(archivePath);
+  core.info(`Successfully extracted fiirefox ${version} to ${extPath}`);
+
+  if (cacheEnabled) {
+    core.info("Adding to the cache ...");
+    const cachedDir = await tc.cacheDir(extPath, "firefox", version);
+    core.info(`Successfully cached firefox ${version} to ${cachedDir}`);
+  }
+  return extPath;
+}
+
+const run = async (): Promise<void> => {
   try {
     const version = core.getInput("firefox-version") || LatestVersion.LATEST;
     const platform = getPlatform();
@@ -13,20 +50,18 @@ async function run(): Promise<void> {
 
     core.info(`Setup firefox ${version} (${language})`);
 
-    const installDir = await installer.install(version, platform, language);
+    const installDir = await install(version, platform, language);
 
     core.addPath(installDir);
     core.info(`Successfully setup firefox version ${version}`);
 
     // output the version actually being used
     const firefoxBin = await io.which("firefox");
-    const fierfoxVersion = (
-      cp.execSync(`${firefoxBin} --version`) || ""
-    ).toString();
+    const fierfoxVersion = cp.execSync(`${firefoxBin} --version`).toString();
     core.info(fierfoxVersion);
   } catch (error) {
     core.setFailed(error.message);
   }
-}
+};
 
 run();
