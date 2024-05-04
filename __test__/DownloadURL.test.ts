e0c4c6d627efe713842c39a86e95f4accf495604
Shin'ya Ueoka <ueokande@i-beam.org>
2024-05-04 13:42:27 +0900

    migrate to vitest

--- __test__/DownloadURL.test.ts
+++ __test__/DownloadURL.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, test } from "vitest";
 import { ArchiveDownloadURL, LatestDownloadURL } from "../src/DownloadURL";
 import { UnsupportedPlatformError } from "../src/errors";
 import { Arch, OS } from "../src/platform";
